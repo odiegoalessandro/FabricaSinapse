f2dbbd80c2c931a0e11d0c6c41971486c8855561
= <=>
2022-10-12 12:04:19 -0300

    feat: sharing data between screens

--- app/src/main/java/com/example/fabricadesinapse/MainActivity.kt
+++ app/src/main/java/com/example/fabricadesinapse/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.example.fabricadesinapse
 
-import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,10 +15,23 @@ class MainActivity : AppCompatActivity() {
         val tvResult = findViewById<TextView>(R.id.tvResult)
         val etName = findViewById<EditText>(R.id.etName)
         val btSend = findViewById<Button>(R.id.btSend)
+        val btSend2 = findViewById<Button>(R.id.btSend2)
 
         btSend.setOnClickListener {
-            if(etName.text.isNotBlank()){
+            if (etName.text.isNotBlank()) {
                 tvResult.text = getString(R.string.hello_name, etName.text.toString())
+            } else {
+                etName.error = getString(R.string.type_your_name)
+            }
+        }
+
+        btSend2.setOnClickListener {
+            if (etName.text.isNotBlank()) {
+                val name = etName.text.toString()
+                val intent = Intent(this, ResultActivity::class.java)
+
+                intent.putExtra("NAME", name)
+                startActivity(intent)
             } else {
                 etName.error = getString(R.string.type_your_name)
             }
--- app/src/main/java/com/example/fabricadesinapse/ResultActivity.kt
+++ app/src/main/java/com/example/fabricadesinapse/ResultActivity.kt
@@ -1,11 +1,17 @@
 package com.example.fabricadesinapse
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+
+        val name = intent.getStringExtra("NAME")
+        val tvResult = findViewById<TextView>(R.id.tvResult)
+
+        tvResult.text = getString(R.string.hello_name, name)
     }
 }
